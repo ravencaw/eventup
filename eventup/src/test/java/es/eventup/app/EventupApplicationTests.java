cb9b40f1acc4626a6fb28194b7d0851787d232de
ravencaw <sebastianpedrosa.iespe@gmail.com>
2020-04-29 14:47:05 +0200

    Add files via upload

---
+++ eventup/src/test/java/es/eventup/app/EventupApplicationTests.java
@@ -0,0 +1,13 @@
+package es.eventup.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
