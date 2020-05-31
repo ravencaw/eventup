03dba7f9e0738e7dbb2b7b7c2414c5f1fc26b309
Adrian Casado <acasgar2@alu.upo.es>
2020-05-31 12:06:06 +0200

    HOME

--- src/main/java/es/eventup/app/controllers/HomeController.java
+++ src/main/java/es/eventup/app/controllers/HomeController.java
@@ -5,13 +5,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import es.eventup.app.models.service.EventoService;
+import es.eventup.app.models.service.UsuarioService;
+import es.eventup.app.util.mail.MailSend;
 
 @Controller
+@SessionAttributes("evento")
+
 public class HomeController {
+	
+	private final EventoService serviceEvento;
+	
+	public HomeController( EventoService serviceEvento) {
+
+		this.serviceEvento = serviceEvento;
+	}
+	
+	
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String listar(Model model) {
 		model.addAttribute("tituloWeb", "Home");
 		model.addAttribute("titulo", "Home");
+		model.addAttribute("eventos", serviceEvento.findAll());
 		return "home";
 	}
 	
